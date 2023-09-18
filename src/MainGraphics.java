804f683a2a2909e0aab96705c838f3b0f20bb069
SOHick <shnikitaprog@mail.ru>
2023-09-18 10:16:56 +0300

    Initial commit

---
+++ src/MainGraphics.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+class MainGraphics extends JFrame
+{
+    public MainGraphics(){
+        Dimension minSize = new Dimension(800, 600);
+        setSize(minSize);
+        setMinimumSize(minSize);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        GroupLayout gl = new GroupLayout(getContentPane()); // Наше окно без рамок
+        setLayout(gl);
+        MainPanel mainPanel = new MainPanel();
+        mainPanel.setBackground(Color.lightGray);
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addGap(8)
+                .addGroup(gl.createParallelGroup()
+                        .addComponent(mainPanel,GroupLayout.DEFAULT_SIZE,GroupLayout.DEFAULT_SIZE,GroupLayout.DEFAULT_SIZE)
+                )
+                .addGap(8)
+        );
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addGap(8)
+                .addGroup(gl.createSequentialGroup()
+                        .addComponent(mainPanel,GroupLayout.DEFAULT_SIZE,400,GroupLayout.DEFAULT_SIZE)
+
+
+                )
+                .addGap(8)
+        );
+    }
+
+}
---
+++ src/Painter.java
@@ -0,0 +1,6 @@
+import java.awt.*;
+
+public interface Painter
+{
+    void paint(Graphics g,int Wight, int Height);
+}
